1c83ae18acc05776a9c6765acb1c476978d95c51
Shafinaz Shafique <shafinaz2006@gmail.com>
2019-10-25 13:29:03 -0700

    Update CSTBExams.java

--- CSTBExams.java
+++ CSTBExams.java
@@ -13,9 +13,9 @@ public class CSTBExams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello Selenium");
+	    System.out.println("hello Selenium");
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium_jars\\ChromeDriver\\chromedriver.exe");
+	    System.setProperty("webdriver.chrome.driver", "C:\\Selenium_jars\\ChromeDriver\\chromedriver.exe");
 		
 	    WebDriver driver = new ChromeDriver();
 	    
@@ -28,7 +28,7 @@ public class CSTBExams {
 	    Actions action = new Actions(driver);
 	    
 		
-		action.moveToElement(driver.findElement(By.xpath("//li[contains(@id, 'menu-item')]//a[contains(text(), 'Exams')]"))).perform();
+	    action.moveToElement(driver.findElement(By.xpath("//li[contains(@id, 'menu-item')]//a[contains(text(), 'Exams')]"))).perform();
 	    
 	    driver.findElement(By.xpath("//ul[@class = 'sub-menu']//a[contains(text(), 'Exam Information')]")).click();
 	    
@@ -58,15 +58,20 @@ public class CSTBExams {
 	    
 	    List<WebElement> bookInfoList = driver.findElements(By.xpath("//h3[contains(text(), 'Advanced Level Books')]//following-sibling::p"));
 	    
+	    String bookInfo;
+	    String bookName;
+            String price;
+	    String author;
+		
 	    for (int i = 0; i < bookInfoList.size(); i++) {
 	    	
-	    	String bookInfo = bookInfoList.get(i).getText();
+	    	bookInfo = bookInfoList.get(i).getText();
 	    
-	    	String bookName = bookInfo.substring(0, bookInfo.indexOf("by"));
+	    	bookName = bookInfo.substring(0, bookInfo.indexOf("by"));
 		
-	    	String price = bookInfo.substring(bookInfo.indexOf("List Price"), bookInfo.indexOf("Language"));
+	    	price = bookInfo.substring(bookInfo.indexOf("List Price"), bookInfo.indexOf("Language"));
 		
-	    	String author = bookInfo.substring(bookInfo.indexOf("by"), bookInfo.indexOf("List Price"));
+	    	author = bookInfo.substring(bookInfo.indexOf("by"), bookInfo.indexOf("List Price"));
 		
 	    	System.out.println("Name of the book is: " + bookName + "\n" + price + "\nAuthor is: " + author);
 	    	
